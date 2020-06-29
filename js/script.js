961bd6a900cbe975ffcacf94bdf6bcafcfbe2ef6
Michael Olshewski <michael.olshewski@yahoo.com>
2020-06-29 01:49:01 -0400

    Added Completed UI / Added Initial script.js / Added Custom CSS File

---
+++ js/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+    var apiKey = "956e3f10f1bb30392cacd6f271ecc9c8";
+
+
+
+    var productionMode = false;
+    if (!productionMode) {
+        console.log("Production Mode? " + productionMode);
+    }
+});
